4ced3cca781c3c5010cf741392f06e6aec20fc59
R4Tobi <R4Tobi@users.noreply.github.com>
2023-10-19 13:36:06 +0200

    added 1.4 Histogramm

---
+++ Utils/Push.java
@@ -0,0 +1,13 @@
+package Utils;
+
+public class Push {
+    private static int[] pushInt(int[] array, int push) {
+        //verlängere den Array um eine Position
+        int[] longer = new int[array.length + 1];
+        //füge die werte dem neuen Array hinzu
+        for (int i = 0; i < array.length; i++) longer[i] = array[i];
+        //setze den neuen Wert an der letzten Position ein
+        longer[array.length] = push;
+        return longer;
+    }
+}
